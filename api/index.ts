81b077e464411a27c0309593cfe0b46a55c7881c
nguyenlejoe <joe_le.1@hotmail.com>
2022-07-05 11:16:47 -0700

    testing post

--- api/index.ts
+++ api/index.ts
@@ -1,31 +1,55 @@
-import { IncomingMessage, ServerResponse } from 'http';
+import { ServerResponse } from 'http';
 import { parseRequest } from './_lib/parser';
 import { getScreenshot } from './_lib/chromium';
 import { getHtml } from './_lib/template';
 const isDev = !process.env.AWS_REGION;
 const isHtmlDebug = process.env.OG_HTML_DEBUG === '1';
 
-export default async function handler(req: IncomingMessage, res: ServerResponse) {
-    try {
-        const parsedReq = parseRequest(req);
-        const html = getHtml(parsedReq);
-        if (isHtmlDebug) {
+export default async function handler(req: any, res: ServerResponse) {
+    if (req.method === 'GET'){
+        try {
+            const parsedReq = parseRequest(req);
+            const html = getHtml(parsedReq);
+            if (isHtmlDebug) {
+                res.setHeader('Content-Type', 'text/html');
+                res.end(html);
+                return;
+            }
+            parsedReq.image = "";
+            const { fileType } = parsedReq;
+            const file = await getScreenshot(html, fileType, isDev);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', `image/${fileType}`);
+            res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
+            res.end(file);
+    
+        } catch (e) {
+            res.statusCode = 500;
             res.setHeader('Content-Type', 'text/html');
-            res.end(html);
-            return;
+            res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
+            console.error(e);
         }
-        parsedReq.image = "";
-        const { fileType } = parsedReq;
-        const file = await getScreenshot(html, fileType, isDev);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', `image/${fileType}`);
-        res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
-        res.end(file);
-
-    } catch (e) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
-        console.error(e);
+    } else{
+        try {
+            const html = getHtml(req.body);
+            if (isHtmlDebug) {
+                res.setHeader('Content-Type', 'text/html');
+                res.end(html);
+                return;
+            }
+            const { fileType } = req.body;
+            const file = await getScreenshot(html, fileType, isDev);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', `image/${fileType}`);
+            res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
+            res.end(file);
+    
+        } catch (e) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
+            console.error(e);
+        }
     }
+    
 }
