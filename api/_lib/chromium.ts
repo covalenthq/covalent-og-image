89044d78c2ecfe3ad8f7e134b9ce68355ace0340
nguyenlejoe <joe_le.1@hotmail.com>
2022-07-25 15:16:49 -0700

    close browser

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -3,35 +3,42 @@ import { getOptions } from './options';
 import { FileType } from './types';
 let _page: core.Page | null;
 
-async function getPage(isDev: boolean) {
-    if (_page) {
-        return _page;
-    }
+// async function getPage(isDev: boolean) {
+//     if (_page) {
+//         return _page;
+//     }
+//     const options = await getOptions(isDev);
+//     const browser = await core.launch(options);
+//     _page = await browser.newPage();
+//     return _page;
+// }
+
+export async function getScreenshot(html: string, type: FileType, isDev: boolean) {
     const options = await getOptions(isDev);
     const browser = await core.launch(options);
     _page = await browser.newPage();
-    return _page;
-}
-
-export async function getScreenshot(html: string, type: FileType, isDev: boolean) {
-    const page = await getPage(isDev);
-    await page.setViewport({ width: 2048, height: 1170 });
-    await page.setContent(html);
-    const file = await page.screenshot({ type });
+    await _page.setViewport({ width: 2048, height: 1170 });
+    await _page.setContent(html);
+    const file = await _page.screenshot({ type });
+    await _page.close();
+    await browser.close();
     return file;
 }
 
 export async function getReactScreenshot(id: string, isDev: boolean) {
-    const page = await getPage(isDev);
-    await page.setViewport({ width: 1920, height: 1080 });
+    const options = await getOptions(isDev);
+    const browser = await core.launch(options);
+    _page = await browser.newPage();
+    await _page.setViewport({ width: 1920, height: 1080 });
 
     const url = `https://covalent-embed.vercel.app/${id}?embed=1234`;
     
-    await page.goto(url, {
+    await _page.goto(url, {
         waitUntil: 'networkidle2'
     });
     
-    const file = await page.screenshot({type: "png" });
-    await page.close();
+    const file = await _page.screenshot({type: "png" });
+    await _page.close();
+    await browser.close();
     return file;
 }
