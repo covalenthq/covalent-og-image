8dd8c55a5ffb581bee49c5ba6aaa70a210101c34
nguyenlejoe <joe_le.1@hotmail.com>
2022-07-25 14:25:35 -0700

    post

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import { ServerResponse } from 'http';
 import { parseRequest } from './_lib/parser';
-import {  getScreenshot } from './_lib/chromium';
+import { getReactScreenshot, getScreenshot } from './_lib/chromium';
 import { getHtml } from './_lib/websiteTemplate';
 const isDev = !process.env.AWS_REGION;
 const isHtmlDebug = process.env.OG_HTML_DEBUG === '1';
@@ -31,24 +31,20 @@ export default async function handler(req: any, res: ServerResponse) {
             res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
             console.error(e);
         }
-    }
-    // } else{
-    //     // const request = JSON.parse(req.body)
-    //     try {
-
-    //         const file = await getReactScreenshot(req.body.id, isDev);
-    //         console.log(file)
-    //         res.statusCode = 200;
-    //         res.setHeader('Content-Type', `image/png`);
-    //         res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
-    //         res.end(file);
+    } else{
+        try {
+            const file = await getReactScreenshot(req.body.id, isDev);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', `image/png`);
+            res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
+            res.end(file);
     
-    //     } catch (e) {
-    //         res.statusCode = 500;
-    //         res.setHeader('Content-Type', 'text/html');
-    //         res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
-    //         console.error(e);
-    //     }
-    // }
+        } catch (e) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
+            console.error(e);
+        }
+    }
     
 }
